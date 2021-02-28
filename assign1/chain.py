ed7cf5396e6587072607cf9885d195b568438b40
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-02-28 14:04:16 -0600

    Added code for chain.py

---
+++ assign1/chain.py
@@ -0,0 +1,37 @@
+#This program can be run using mpi with any number of processes. 
+#The root process (process 0) will get input from the user in a loop until a negative number is encountered
+#Each number input by the user will get passed along a chain of processes, where process i will get the 
+#number from i-1, then pass it to i+1. At every process, the number and rank of the process will be printed out.
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+if rank == 0:
+    while True:
+        value = int(input('Enter a number: '))
+        if value < 0:
+            comm.send(value, dest=rank + 1, tag = rank + 1)
+            exit()
+        else:
+            print ("Process %d got %d" % (rank, value))
+            comm.send(value, dest=rank + 1, tag = rank + 1)
+elif rank == size - 1:
+    while True:
+        value = comm.recv(source = rank - 1, tag = rank-1)
+        if value < 0:
+            exit()
+        else:
+            print ("Process %d got %d" % (rank, value))
+else:
+    while True:
+        value = comm.recv(source = rank - 1, tag = rank-1)
+        if value < 0:
+            comm.send(value, dest=rank + 1, tag = rank + 1)
+            exit()
+        else:
+            print ("Process %d got %d" % (rank, value))
+            comm.send(value, dest=rank + 1, tag = rank + 1)
