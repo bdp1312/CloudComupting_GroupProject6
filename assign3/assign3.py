5d675bd5d62c7f7c9141b7f004da8d24658f9919
Chameleon Cloud User <cc@group6-1.novalocal>
2021-04-12 17:53:36 +0000

    got top 3 crimes ranked

--- assign3/assign3.py
+++ assign3/assign3.py
@@ -13,7 +13,7 @@ data = sc.textFile("hdfs://10.56.2.183:54310/hw2-input/NYPD_Complaint_Data_Curre
 # This uses csv reader to split each line of the file, then filter for July (row[5] is RPT_DT column)
 splitdata = data.mapPartitions(lambda x: reader(x)).filter(lambda row: row[5].startswith('07'))
 #Count the number of each crime committed (row[7] is OFNS_DESC column)
-topCrimes = splitdata.map(lambda row: (row[7], 1)).reduceByKey(lambda a, b: a + b)
+topCrimes = splitdata.map(lambda row: (row[7], 1)).reduceByKey(lambda a, b: a + b).sortBy(lambda row: row[1], ascending=False)
 
 output = topCrimes.collect()
 
